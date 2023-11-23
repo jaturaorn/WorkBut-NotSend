6f5fe3ab92350a87490f8d222595665ad1f02254
Fdev2023 <jaturapornff@gmail.com>
2023-11-23 10:27:27 +0700

    Finish in Trend page

--- src/app/trend/page.js
+++ src/app/trend/page.js
@@ -1,12 +1,22 @@
 import Layout from "../component/layout";
-import Link from "next/link";
+
+import LineChart from "../component/Chart/LineChart";
 
 const page = () => {
   return (
     <Layout>
-      <div className="flex justify-center">
-        <h1>Trend</h1>
-        <Link href="/">← Back to home</Link>
+      <div className="flex justify-start ml-[165px]">
+        <div
+          className="w-[1350px] h-screen shadow-lg flex p-2 
+border border-slate-300 bg-white mt-2"
+        >
+          <div
+            className="m-auto w-full max-sm:w-[355px] 
+            max-sm:h-[200px] max-lg:w-[750px]"
+          >
+            <LineChart />
+          </div>
+        </div>
       </div>
     </Layout>
   );
